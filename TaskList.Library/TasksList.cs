2979af3419608719873aaf1eef8ae511ea5ffc63
Mozzey <mbcacciano@gmail.com>
2018-07-30 00:32:43 -0400

    added comments removed white space

--- TaskList.Library/TasksList.cs
+++ TaskList.Library/TasksList.cs
@@ -18,84 +18,36 @@ namespace TaskList.Library
             Console.WriteLine("Task Entered!");
             this.Add(new Task(teamMember, description, dueDate));
         }
-        // Display all tasks
-        public void DisplayList()
+        // Edit a current task
+        public void EditTask()
         {
-            string categories = String.Format("{0, -3}{1,-10}{2,-14}{3,-21}{4,-22}", "#", "Complete", "Due Date",
-                "Team Member", "Description");
-            Console.WriteLine(categories);
-            Console.WriteLine("  -------------------------------------------------------------");
+            DisplayList();
+            Console.Write("Please enter a task number to be edited: ");
+            var taskNumber = IsValidInt();
             for (int i = 0; i < this.Count; i++)
             {
-                this.DisplayTask(i);
-            }
-        }
-        // Display single task by index number
-        public void DisplayTask(int i)
-        {
-            string formattedTask = String.Format("{0,-4} {1,-7} {2,-14} {3, -20} {4, -25}",
-                $"{i + 1}.", this[i].Completed, this[i].DueDate, this[i].TeamMember, this[i].Description);
-            Console.WriteLine(formattedTask);
-        }
-        // Display a list of tasks by team member name
-        public void DisplayTeamMemberTasks()
-        {
-            Console.Write("Please enter a team member name to see their specific tasks: ");
-            var teamMember = IsValidName();
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (teamMember == this[i].TeamMember)
+                if (taskNumber == (i + 1))
                 {
-                    DisplayTask(i);
-                }
-            }
-        }
-        // Display the main menu
-        public string MainMenu()
-        {
-            Console.WriteLine("Welcome to the Task Manager!");
-            String mainMenu = String.Format("{0,19}\n{1,15}\n{2,19}\n{3,25}\n{4,16}\n{5,11}",
-                "1. List options",
-                "2. Add task",
-                "3. Delete tasks",
-                "4. Mark task complete",
-                "5. Edit Task",
-                "6. Quit");
-            return mainMenu;
-        }
-        // Sub menu to choose how you would like to display task(s)
-        public string ListDisplayMenu()
-        {
-            Console.WriteLine("Please choose an option: ");
-            String listMenu = String.Format("{0,21}\n{1,27}\n{2,28}\n{3,16}",
-                "1. List all tasks",
-                "2. Team Member specific",
-                "3. Before specified date",
-                "4. Main Menu");
-            Console.WriteLine(listMenu);
-            var option = Console.ReadLine();
-            if (int.TryParse(option, out int validOption))
-            {
-                switch (validOption)
-                {
-                    case 1:
-                        this.DisplayList();
-                        break;
-                    case 2:
-                        this.DisplayTeamMemberTasks();
-                        break;
-                    case 3:
-                        Console.WriteLine("wip");
-                        break;
-                    case 4:
+                    Console.WriteLine("Are you sure you want to edit this task? (Y/N)");
+                    this.DisplayTask(i);
+                    var confirmation = Console.ReadLine().ToUpper();
+                    if (confirmation.StartsWith("Y"))
+                    {
+
+                        Console.Write("Team member name: ");
+                        this[i].TeamMember = IsValidName();
+                        Console.Write("Task Description: ");
+                        this[i].Description = Console.ReadLine();
+                        Console.Write("Due Date: ");
+                        this[i].DueDate = IsValidDate();
+                        Console.WriteLine("Task Edited!");
+                    }
+                    else if (confirmation.StartsWith("N"))
+                    {
                         MainMenu();
-                        break;
-                    default:
-                        ListDisplayMenu();
-                        break;
+                    }
                 }
             }
-            return listMenu;
         }
         // Delete single task by index
         public void DeleteTask()
@@ -159,6 +111,112 @@ namespace TaskList.Library
                 }
             }
         }
+        // Display the main menu
+        public string MainMenu()
+        {
+            Console.WriteLine("Welcome to the Task Manager!");
+            String mainMenu = String.Format("{0,19}\n{1,15}\n{2,19}\n{3,25}\n{4,16}\n{5,11}",
+                "1. List options",
+                "2. Add task",
+                "3. Delete tasks",
+                "4. Mark task complete",
+                "5. Edit Task",
+                "6. Quit");
+            return mainMenu;
+        }
+        // Sub menu to choose how you would like to display task(s)
+        public void ListDisplayMenu()
+        {
+            Console.WriteLine("Please choose an option: ");
+            String listMenu = String.Format("{0,21}\n{1,27}\n{2,28}\n{3,16}",
+                "1. List all tasks",
+                "2. Team Member specific",
+                "3. Before specified date",
+                "4. Main Menu");
+            Console.WriteLine(listMenu);
+            var option = Console.ReadLine();
+            if (int.TryParse(option, out int validOption))
+            {
+                switch (validOption)
+                {
+                    case 1:
+                        this.DisplayList();
+                        break;
+                    case 2:
+                        this.DisplayTeamMemberTasks();
+                        break;
+                    case 3:
+                        this.DisplayTasksBeforeGivenDate();
+                        break;
+                    case 4:
+                        MainMenu();
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please choose and option from the list");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        ListDisplayMenu();
+                        break;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please choose and option from the list");
+                Console.ForegroundColor = ConsoleColor.White;
+                ListDisplayMenu();
+            }
+        }
+        // Display all tasks
+        public void DisplayList()
+        {
+            string categories = String.Format("{0, -3}{1,-10}{2,-14}{3,-21}{4,-22}", "#", "Complete", "Due Date",
+                "Team Member", "Description");
+            Console.WriteLine(categories);
+            Console.WriteLine("  -------------------------------------------------------------");
+            for (int i = 0; i < this.Count; i++)
+            {
+                this.DisplayTask(i);
+            }
+        }
+        // Display single task by index number
+        public void DisplayTask(int i)
+        {
+            string formattedTask = String.Format("{0,-4} {1,-7} {2,-14} {3, -20} {4, -25}",
+                $"{i + 1}.", this[i].Completed, this[i].DueDate, this[i].TeamMember, this[i].Description);
+            Console.WriteLine(formattedTask);
+        }
+        // Display a list of tasks by team member name
+        public void DisplayTeamMemberTasks()
+        {
+            Console.Write("Please enter a team member name to see their specific tasks: ");
+            var teamMember = IsValidName();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (teamMember == this[i].TeamMember)
+                {
+                    DisplayTask(i);
+                }
+            }
+        }
+        // Displays tasks before given date
+        public void DisplayTasksBeforeGivenDate()
+        {
+            Console.Write("Please enter a date to see all tasks due before that date(mm/dd/yyyy): ");
+            var dueDate = Console.ReadLine();
+            if (DateTime.TryParse(dueDate, out DateTime validDate))
+            {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    DateTime formatDueDate = DateTime.Parse(this[i].DueDate);
+                    if (DateTime.Compare(formatDueDate, validDate) < 0)
+                    {
+                        DisplayTask(i);
+                    }
+                }
+            }
+            
+        }
         // Check for valid integer and that input is within range of current tasks
         public int IsValidInt()
         {
@@ -211,36 +269,6 @@ namespace TaskList.Library
                 return IsValidName();
             }
         }
-        // Edit a current task
-        public void EditTask()
-        {
-            DisplayList();
-            Console.Write("Please enter a task number to be edited: ");
-            var taskNumber = IsValidInt();
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (taskNumber == (i + 1))
-                {
-                    Console.WriteLine("Are you sure you want to edit this task? (Y/N)");
-                    this.DisplayTask(i);
-                    var confirmation = Console.ReadLine().ToUpper();
-                    if (confirmation.StartsWith("Y"))
-                    {
-                        
-                        Console.Write("Team member name: ");
-                        this[i].TeamMember = IsValidName();
-                        Console.Write("Task Description: ");
-                        this[i].Description = Console.ReadLine();
-                        Console.Write("Due Date: ");
-                        this[i].DueDate = IsValidDate();
-                        Console.WriteLine("Task Edited!");
-                    }
-                    else if (confirmation.StartsWith("N"))
-                    {
-                        MainMenu();
-                    }
-                }
-            }
-        }
+        
     }
 }
--- TaskListApp/Program.cs
+++ TaskListApp/Program.cs
@@ -9,42 +9,74 @@ namespace TaskListApp
     {
         static void Main(string[] args)
         {
-            var taskList = new TasksList(); 
+            // Instantiate a new task list
+            var taskList = new TasksList();
+            ////////////////////////////////////////////////////////
+            ///////////////////// TEST TASKS ///////////////////////
+            Task task1 = new Task("mike", "mike's task", "01/01/19");
+            Task task2 = new Task("erik", "erik's task", "02/02/19");
+            Task task3 = new Task("mike", "mike's task 2", "01/20/20");
+            Task task4 = new Task("mike", "mike's task 3", "01/03/19");
+            taskList.Add(task1);
+            taskList.Add(task2);
+            taskList.Add(task3);
+            taskList.Add(task4);
+            ///////////////////////////////////////////////////////
+            // flag for main loop
             bool isRunning = true;
+            // Main program loop
             while (isRunning)
             {
                 Console.ForegroundColor = ConsoleColor.White;
+                // Display main menu
                 string mainMenu = taskList.MainMenu();
                 Console.WriteLine(mainMenu);
+                // Ask user to choose an option
                 Console.Write("What would you like to do? ");
                 string option = Console.ReadLine();
+                // validate that input is an intgeter
                 if (int.TryParse(option, out int validOption))
                 {
+                    // Options from the main menu
                     switch (validOption)
                     {
+                        // If option 1 display sub menu with listing options
                         case 1:
-                            var listOption = taskList.ListDisplayMenu();
+                            // sub menu with listing options(see TaskList.cs -> ListDisplayMenu())
+                            taskList.ListDisplayMenu();
                             break;
+                        // If option 2 ask user to input task members and add a new task to the list
                         case 2:
                             taskList.AddTask();
                             break;
+                        // If option 3 ask user to choose a task to delete, ask user to confirm, delete task
                         case 3:
                             taskList.DeleteTask();
                             break;
+                        // If option 4 ask which task user would like to mark complete, ask user to confirm, mark task complete
                         case 4:
                             taskList.MarkTaskComplete();
                             break;
+                        // If option 5 ask which task user would like to edit, ask user to confirm, input new task members, edit task
                         case 5:
                             taskList.EditTask();
                             break;
+                        // If option 6 say goodbye and end program
                         case 6:
                             Console.WriteLine("Have a Great Day!!");
                             Console.ReadKey();
                             isRunning = false;
                             break;
                         default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Please choose and option from the list");
                             break;
                     }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please choose and option from the list");
                 }
             }
         }
