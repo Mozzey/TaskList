6375da750269beae77a6beaa7636c0d136df9d99
Mozzey <mbcacciano@gmail.com>
2018-07-29 23:10:38 -0400

    edit task and list task by team member name done

--- TaskList.Library/TasksList.cs
+++ TaskList.Library/TasksList.cs
@@ -6,6 +6,7 @@ namespace TaskList.Library
 {
     public class TasksList : List<Task>
     {
+        // Add a new task
         public void AddTask()
         {
             Console.Write("Team member name: ");
@@ -17,7 +18,7 @@ namespace TaskList.Library
             Console.WriteLine("Task Entered!");
             this.Add(new Task(teamMember, description, dueDate));
         }
-
+        // Display all tasks
         public void DisplayList()
         {
             string categories = String.Format("{0, -3}{1,-10}{2,-14}{3,-21}{4,-22}", "#", "Complete", "Due Date",
@@ -29,23 +30,74 @@ namespace TaskList.Library
                 this.DisplayTask(i);
             }
         }
+        // Display single task by index number
         public void DisplayTask(int i)
         {
             string formattedTask = String.Format("{0,-4} {1,-7} {2,-14} {3, -20} {4, -25}",
                 $"{i + 1}.", this[i].Completed, this[i].DueDate, this[i].TeamMember, this[i].Description);
             Console.WriteLine(formattedTask);
         }
+        // Display a list of tasks by team member name
+        public void DisplayTeamMemberTasks()
+        {
+            Console.Write("Please enter a team member name to see their specific tasks: ");
+            var teamMember = IsValidName();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (teamMember == this[i].TeamMember)
+                {
+                    DisplayTask(i);
+                }
+            }
+        }
+        // Display the main menu
         public string MainMenu()
         {
             Console.WriteLine("Welcome to the Task Manager!");
-            String mainMenu = String.Format("{0,17}\n{1,15}\n{2,19}\n{3,25}\n{4,11}",
-                "1. List tasks",
+            String mainMenu = String.Format("{0,19}\n{1,15}\n{2,19}\n{3,25}\n{4,16}\n{5,11}",
+                "1. List options",
                 "2. Add task",
                 "3. Delete tasks",
                 "4. Mark task complete",
-                "5. Quit");
+                "5. Edit Task",
+                "6. Quit");
             return mainMenu;
         }
+        // Sub menu to choose how you would like to display task(s)
+        public string ListDisplayMenu()
+        {
+            Console.WriteLine("Please choose an option: ");
+            String listMenu = String.Format("{0,21}\n{1,27}\n{2,28}\n{3,16}",
+                "1. List all tasks",
+                "2. Team Member specific",
+                "3. Before specified date",
+                "4. Main Menu");
+            Console.WriteLine(listMenu);
+            var option = Console.ReadLine();
+            if (int.TryParse(option, out int validOption))
+            {
+                switch (validOption)
+                {
+                    case 1:
+                        this.DisplayList();
+                        break;
+                    case 2:
+                        this.DisplayTeamMemberTasks();
+                        break;
+                    case 3:
+                        Console.WriteLine("wip");
+                        break;
+                    case 4:
+                        MainMenu();
+                        break;
+                    default:
+                        ListDisplayMenu();
+                        break;
+                }
+            }
+            return listMenu;
+        }
+        // Delete single task by index
         public void DeleteTask()
         {
 
@@ -82,7 +134,7 @@ namespace TaskList.Library
                 DeleteTask();
             }
         }
-
+        // Mark single task as complete
         public void MarkTaskComplete()
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -107,7 +159,7 @@ namespace TaskList.Library
                 }
             }
         }
-
+        // Check for valid integer and that input is within range of current tasks
         public int IsValidInt()
         {
             var taskNumber = Console.ReadLine();
@@ -124,7 +176,7 @@ namespace TaskList.Library
                 return IsValidInt();
             }
         }
-
+        // Validates due date input MM/dd/yyyy and makes sure due date is not before the current day
         public string IsValidDate()
         {
             var dueDate = Console.ReadLine();
@@ -141,7 +193,7 @@ namespace TaskList.Library
                 return IsValidDate();
             }
         }
-
+        // Validates team member name - either "F(f)irstname" or "F(f)irstname L(l)astname"
         public string IsValidName()
         {
             var validName = @"[A-Za-z]+\s?[A-Za-z]+";
@@ -157,6 +209,37 @@ namespace TaskList.Library
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Team Member Name:");
                 return IsValidName();
+            }
+        }
+        // Edit a current task
+        public void EditTask()
+        {
+            DisplayList();
+            Console.Write("Please enter a task number to be edited: ");
+            var taskNumber = IsValidInt();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (taskNumber == (i + 1))
+                {
+                    Console.WriteLine("Are you sure you want to edit this task? (Y/N)");
+                    this.DisplayTask(i);
+                    var confirmation = Console.ReadLine().ToUpper();
+                    if (confirmation.StartsWith("Y"))
+                    {
+                        
+                        Console.Write("Team member name: ");
+                        this[i].TeamMember = IsValidName();
+                        Console.Write("Task Description: ");
+                        this[i].Description = Console.ReadLine();
+                        Console.Write("Due Date: ");
+                        this[i].DueDate = IsValidDate();
+                        Console.WriteLine("Task Edited!");
+                    }
+                    else if (confirmation.StartsWith("N"))
+                    {
+                        MainMenu();
+                    }
+                }
             }
         }
     }
--- TaskListApp/Program.cs
+++ TaskListApp/Program.cs
@@ -17,29 +17,34 @@ namespace TaskListApp
                 string mainMenu = taskList.MainMenu();
                 Console.WriteLine(mainMenu);
                 Console.Write("What would you like to do? ");
-                int option = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-                switch (option)
+                string option = Console.ReadLine();
+                if (int.TryParse(option, out int validOption))
                 {
-                    case 1:
-                        taskList.DisplayList();
-                        break;
-                    case 2:
-                        taskList.AddTask();
-                        break;
-                    case 3:
-                        taskList.DeleteTask();
-                        break;
-                    case 4:
-                        taskList.MarkTaskComplete();
-                        break;
-                    case 5:
-                        Console.WriteLine("Have a Great Day!!");
-                        Console.ReadKey();
-                        isRunning = false;
-                        break;
-                    default:
-                        break;
+                    switch (validOption)
+                    {
+                        case 1:
+                            var listOption = taskList.ListDisplayMenu();
+                            break;
+                        case 2:
+                            taskList.AddTask();
+                            break;
+                        case 3:
+                            taskList.DeleteTask();
+                            break;
+                        case 4:
+                            taskList.MarkTaskComplete();
+                            break;
+                        case 5:
+                            taskList.EditTask();
+                            break;
+                        case 6:
+                            Console.WriteLine("Have a Great Day!!");
+                            Console.ReadKey();
+                            isRunning = false;
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
